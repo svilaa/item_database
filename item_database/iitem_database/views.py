d0db777caf54d68040df836fa0e06c0dfef413ce
Dídac <didac.semente@gmail.com>
2014-03-25 20:54:27 +0100

    Implemented JSON as a valid HTTP response file type

--- item_database/iitem_database/views.py
+++ item_database/iitem_database/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+import json as jsn
+
 from django.http import HttpResponse, Http404, HttpResponseNotFound
 from django.template import Context
 from django.template.loader import get_template
@@ -8,6 +10,8 @@ from django.contrib.auth.models import User
 html = 'html'
 json = 'json'
 xml = 'xml'
+
+json_indent_level = 4
 
 def mainpage(request):
 	template = get_template('mainpage.html')
@@ -25,9 +29,10 @@ def userpage(request, username, format=html):
 	except:
 		raise Http404('User not found.')
 
+	userItems = UserItems.objects.filter(userID=user.id)
+
 	if format == html:
 		template = get_template('userpage.html')
-		userItems = UserItems.objects.filter(userID=user.id)
 		variables = Context({
 			'username': username,
 			'userItems': userItems,
@@ -35,14 +40,21 @@ def userpage(request, username, format=html):
 		output = template.render(variables)
 		return HttpResponse(output)
 	elif format == json:
-		return HttpResponseNotFound('json')
+		json_response = {}
+		json_response['User ID'] = user.id
+		json_response['User Name'] = user.username
+		json_response['User Items'] = \
+			[ {'Item ID' : userItem.itemID.id, 'Item Name' : userItem.itemID.name } for userItem in userItems ]
+		return HttpResponse(jsn.dumps(json_response, indent=json_indent_level, separators=(',', ' : ')), 
+			content_type="application/json")
 	else:
 		return HttpResponseNotFound('format not found')
 
 def createList(typeList, titlehead, listUrl, format):
+
+	lis = typeList.objects.all()
 	if format == html:
 		template = get_template('list.html')
-		lis = typeList.objects.all()
 		variables = Context({
 			'titlehead': titlehead,
 			'element_list': lis,
@@ -51,7 +63,10 @@ def createList(typeList, titlehead, listUrl, format):
 		output = template.render(variables)
 		return HttpResponse(output)
 	elif format == json:
-		return HttpResponseNotFound('json')
+		json_response = {}
+		json_response[titlehead] = [ { (titlehead + ' ID') : elem.id , titlehead + ' Name' : elem.name } for elem in lis ]
+		return HttpResponse(jsn.dumps(json_response, indent=json_indent_level, separators=(',', ' : ')), 
+			content_type="application/json")
 	else:
 		return HttpResponseNotFound('format not found')
 
@@ -72,11 +87,12 @@ def itemPage(request, itemID, format=html):
 		item = Item.objects.get(id=itemID)
 	except:
 		raise Http404('Item not found.')
+
+	areas = Area.objects.filter(found__itemID=item.id)
+	drops = Drops.objects.filter(itemID=itemID)
 	
 	if format == html:
 		template = get_template('itemPage.html')
-		areas = Area.objects.filter(found__itemID=item.id)
-		drops = Drops.objects.filter(itemID=itemID)
 		variables = Context({
 			'titlehead': 'Item',
 			'item': item,
@@ -86,7 +102,16 @@ def itemPage(request, itemID, format=html):
 		output = template.render(variables)
 		return HttpResponse(output)
 	elif format == json:
-		return HttpResponseNotFound('json')
+		json_response = {}
+		json_response['Item ID'] = item.id
+		json_response['Item Name'] = item.name 
+		json_response['Description'] = item.desc
+		json_response['Found in'] = [ { 'Area ID' : area.id, 'Area name' : area.name } for area in areas ]
+		json_response['Dropped by'] = [ { 'Creature ID' : drop.creatureID.id, 
+			'Creature Name' : drop.creatureID.name , 'Drop Rate' : drop.dropRate } for drop in drops ]
+
+		return HttpResponse(jsn.dumps(json_response, indent=json_indent_level, separators=(',', ' : ')), 
+			content_type="application/json")
 	else:
 		return HttpResponseNotFound('format not found')
 
@@ -96,9 +121,10 @@ def itemClassPage(request, classItemID, format=html):
 	except:
 		raise Http404('Item class not found.')
 
+	items = Item.objects.filter(typeID=itemClass.id)
+
 	if format == html:
 		template = get_template('itemClassPage.html')
-		items = Item.objects.filter(typeID=itemClass.id)
 		variables = Context({
 			'titlehead': 'Item class',
 			'itemClass': itemClass,
@@ -107,7 +133,14 @@ def itemClassPage(request, classItemID, format=html):
 		output = template.render(variables)
 		return HttpResponse(output)
 	elif format == json:
-		return HttpResponseNotFound('json')
+		json_response = {}
+		json_response['Class ID'] = itemClass.id
+		json_response['Class Name'] = itemClass.name
+		json_response['Class Description'] = itemClass.desc
+		json_response['Items in class'] = [ {'Item ID' : item.id, 'Item Name' : item.name} for item in items]
+
+		return HttpResponse(jsn.dumps(json_response, indent=json_indent_level, separators=(',', ' : ')), 
+			content_type="application/json")
 	else:
 		return HttpResponseNotFound('format not found')
 
@@ -117,10 +150,11 @@ def creaturePage(request, creatureID, format=html):
 	except:
 		raise Http404('Creature not found.')
 
+	drops = Drops.objects.filter(creatureID=creatureID)
+	areas = creature.areas.all()
+
 	if format == html:
 		template = get_template('creaturePage.html')
-		drops = Drops.objects.filter(creatureID=creatureID)
-		areas = creature.areas.all()
 		variables = Context({
 			'titlehead': 'Creature',
 			'creature': creature,
@@ -130,7 +164,18 @@ def creaturePage(request, creatureID, format=html):
 		output = template.render(variables)
 		return HttpResponse(output)
 	elif format == json:
-		return HttpResponseNotFound('json')
+		json_response = {}
+		json_response['Creature ID'] = creature.id
+		json_response['Creature Name'] = creature.name
+		json_response['Is Unique?'] = creature.unique
+		json_response['Danger Level'] = creature.dangerLevel
+		json_response['Souls Obtained'] = creature.souls
+		json_response['Drops'] = \
+			[ {'Item ID' : drop.itemID.id, 'Item Name' : drop.itemID.name, 'Drop Rate' : drop.dropRate} for drop in drops ]
+		json_response['Found In'] = [ {'Area ID' : area.id, 'Area Name' : area.name} for area in areas ]
+
+		return HttpResponse(jsn.dumps(json_response, indent=json_indent_level, separators=(',', ' : ')), 
+			content_type="application/json")
 	else:
 		return HttpResponseNotFound('format not found')
 
@@ -139,11 +184,12 @@ def areaPage(request, areaID, format=html):
 		area = Area.objects.get(id=areaID)
 	except:
 		raise Http404('Area not found.')
+
+	items = Item.objects.filter(found__areaID=area.id)
+	creatures = Creature.objects.filter(areas=area.id)
 		
 	if format == html:
 		template = get_template('areaPage.html')
-		items = Item.objects.filter(found__areaID=area.id)
-		creatures = Creature.objects.filter(areas=area.id)
 		variables = Context({
 			'titlehead': 'Area',
 			'area': area,
@@ -153,6 +199,13 @@ def areaPage(request, areaID, format=html):
 		output = template.render(variables)
 		return HttpResponse(output)
 	elif format == json:
-		return HttpResponseNotFound('json')
+		json_response = {}
+		json_response['Area ID'] = area.id
+		json_response['Area Name'] = area.name
+		json_response['Objects Found'] = [ {'Item ID' : item.id,'Item Name' : item.name} for item in items ]
+		json_response['Creatures Found'] = \
+			[ {'Creature ID' : creature.id ,'Creature Name' : creature.name } for creature in creatures ]
+		return HttpResponse(jsn.dumps(json_response, indent=json_indent_level, separators=(',', ' : ')),
+			content_type="application/json")
 	else:
 		return HttpResponseNotFound('format not found')
